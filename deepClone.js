c855ceda691ca118eb6853eaa23824363bb65e7f
唐文静 <tangwenjing@tangwenjingdeMacBook-Pro.local>
2021-05-24 14:30:24 +0800

    深克隆

---
+++ deepClone.js
@@ -0,0 +1,18 @@
+function deepClone (obj) {
+  if (typeof obj !== 'object' || obj == null) {
+    return obj;
+  }
+
+  let result
+  if (obj instanceof Array) {
+    result = [];
+  } else {
+    result = {};
+  }
+
+  for (const key in obj) {
+    result[key] = deepClone(obj[key]);
+  }
+
+  return result;
+}
